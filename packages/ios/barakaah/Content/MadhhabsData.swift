ca59210e15fe613c309b75a3477c8b2b0c9cfb25
Sana <88476413+souravsspace@users.noreply.github.com>
2026-04-26 23:04:30 +0600

    feat(swift): add static madhhabs data

---
+++ packages/ios/barakaah/Content/MadhhabsData.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+let MADHHABS: [Madhhab] = [
+    Madhhab(id: "hanafi", name: "Hanafi", sub: "South & Central Asia, Turkey"),
+    Madhhab(id: "shafii", name: "Shāfiʿī", sub: "East Africa, SE Asia"),
+    Madhhab(id: "maliki", name: "Mālikī", sub: "North & West Africa"),
+    Madhhab(id: "hanbali", name: "Ḥanbalī", sub: "Arabian Peninsula"),
+    Madhhab(id: "jafari", name: "Jaʿfarī", sub: "Shia tradition"),
+]
