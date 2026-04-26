9cd34b9facf79f633dd65b2b11624cdf6c3e3f75
Sana <88476413+souravsspace@users.noreply.github.com>
2026-04-26 23:03:00 +0600

    feat(swift): add static surahs data

---
+++ packages/ios/barakaah/Content/SurahsData.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+let SURAHS: [Surah] = [
+    Surah(num: 1, name: "Al-Fātiḥah", arabic: "الفاتحة", ayat: 7, place: "Meccan"),
+    Surah(num: 2, name: "Al-Baqarah", arabic: "البقرة", ayat: 286, place: "Medinan"),
+    Surah(num: 3, name: "Āli ʿImrān", arabic: "آل عمران", ayat: 200, place: "Medinan"),
+    Surah(num: 4, name: "An-Nisāʾ", arabic: "النساء", ayat: 176, place: "Medinan"),
+    Surah(num: 18, name: "Al-Kahf", arabic: "الكهف", ayat: 110, place: "Meccan"),
+    Surah(num: 36, name: "Yā-Sīn", arabic: "يس", ayat: 83, place: "Meccan"),
+    Surah(num: 55, name: "Ar-Raḥmān", arabic: "الرحمن", ayat: 78, place: "Medinan"),
+    Surah(num: 67, name: "Al-Mulk", arabic: "الملك", ayat: 30, place: "Meccan"),
+    Surah(num: 112, name: "Al-Ikhlāṣ", arabic: "الإخلاص", ayat: 4, place: "Meccan"),
+    Surah(num: 113, name: "Al-Falaq", arabic: "الفلق", ayat: 5, place: "Meccan"),
+    Surah(num: 114, name: "An-Nās", arabic: "الناس", ayat: 6, place: "Meccan"),
+]
