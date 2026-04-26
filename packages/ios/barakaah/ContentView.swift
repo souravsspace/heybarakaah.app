7b038c43bda17c6665362b31f40d00ebbc112d05
Sana <88476413+souravsspace@users.noreply.github.com>
2026-04-26 16:07:01 +0600

    feat(ios): add iOS project

---
+++ packages/ios/barakaah/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  barakaah
+//
+//  Created by Sourav Ukil on 26/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, barakaah!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ packages/ios/barakaah/barakaahApp.swift
@@ -0,0 +1,17 @@
+//
+//  barakaahApp.swift
+//  barakaah
+//
+//  Created by Sourav Ukil on 26/4/26.
+//
+
+import SwiftUI
+
+@main
+struct barakaahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
